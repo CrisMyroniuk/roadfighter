d186ce67bd03ebd155e0f2ac28efec61ee39f44e
contrerasagustin <contreras.agustin.1999@gmail.com>
2022-05-09 21:52:38 -0300

    Revert

--- src/main/java/roadfighter/Vehicle.java
+++ src/main/java/roadfighter/Vehicle.java
@@ -4,9 +4,7 @@ public abstract class Vehicle extends MapObject {
 	private double speed;
 
 	public double getSpeed() {
-		System.out.println("Error");
 		return speed;
-		System.out.println("casaaa");
 	}
 
 	public void setSpeed(double speed) {
