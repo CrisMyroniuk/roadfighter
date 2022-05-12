80c146a49da5811f9d9d0c6aacce756021f4e1bb
FedePucci1 <fedepucci80@gmailcom>
2022-05-12 17:32:10 -0300

    enemy

--- src/main/java/roadfighter/Enemy.java
+++ src/main/java/roadfighter/Enemy.java
@@ -2,4 +2,14 @@ package roadfighter;
 
 public class Enemy extends Vehicle{
 	private Enum orientation;
+	private boolean visible;
+	
+	public Enemy(double x, double y) {
+		super();
+		this.orientation = orientation;
+		this.visible = visible;
+		this.setX(x);
+		this.setY(y);
+	}
 }
+
