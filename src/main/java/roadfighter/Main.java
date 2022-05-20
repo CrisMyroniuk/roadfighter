e6662e87671c6bd6538cb8413befd39622948bc2
Cristian <nahuelmyroniuk1999@gmail.com>
2022-05-20 19:07:56 -0300

    Java FX

---
+++ src/main/java/roadfighter/Main.java
@@ -0,0 +1,30 @@
+package roadfighter;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Group root = new Group();
+		Scene currentScene = new Scene(root);
+		
+		Image background = new Image("background.png");
+		ImageView imageView = new ImageView(background);
+		root.getChildren().add(imageView);
+		
+		stage.setScene(currentScene);
+		stage.setTitle("Roadfighter");
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
