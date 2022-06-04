9f70c6682ee8d44c5c63f125f883e0b9f9e5342c
Cristian <nahuelmyroniuk1999@gmail.com>
2022-06-03 23:54:53 -0300

    fix bug borde

--- src/main/java/roadfighter/objects/LeftLimit.java
+++ src/main/java/roadfighter/objects/LeftLimit.java
@@ -5,6 +5,6 @@ import roadfighter.Config;
 public class LeftLimit extends DeathLimit {
 	
 	public LeftLimit() {
-		super(400, -10, 10, Config.baseHeight + 20);
+		super(410, -10, 10, Config.baseHeight + 20);
 	}
 }
